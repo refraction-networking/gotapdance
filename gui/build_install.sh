3b3cc239d7a3b0e7b3b42722cc099c321ac34512
Sergey Frolov <sergey.frolov@colorado.edu>
2016-11-24 13:04:46 -0700

    Update pure GOLANG installation script

--- gui/build_install.sh
+++ gui/build_install.sh
@@ -1,5 +1,4 @@
 TARGET="android"
 NAME='gui'
 rm ${NAME}.apk
-gomobile install -target $TARGET -o ${NAME}.apk gitlab.decoyrouting.com/decoy/tapdance/gotapdance/gui
-#gomobile install -target $TARGET gitlab.decoyrouting.com/decoy/tapdance/gotapdance/gui
+gomobile install -target $TARGET -o ${NAME}.apk github.com/SergeyFrolov/gotapdance/gui
