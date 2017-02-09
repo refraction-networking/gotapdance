3a8373162b595a200ad720deb700cfd3a2b7cbc9
Sergey Frolov <sergey.frolov@colorado.edu>
2017-02-08 20:54:59 -0700

    Cleanup flow logic.

--- tapdance/flow.go
+++ tapdance/flow.go
@@ -56,64 +56,51 @@ func (TDstate *TapDanceFlow) Redirect() (err error) {
 	}()
 
 	forwardFromServerToClient := func ()() {
-		b := make([]byte, 16 * 1024 + 20 + 20 + 12)
-		for !TDstate.proxy.stop {
-			n, err := TDstate.servConn.Read(b)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			if n > 0 {
-				TDstate.userConn.SetWriteDeadline(time.Now().Add(time.Second * 10))
-				sent_n, err := TDstate.userConn.Write(b[:n])
-				if err != nil {
-					errChan <- err
-					return
-				}
-				if n != sent_n {
-					err = errors.New("Expected to write " + strconv.Itoa(sent_n) +
-					" bytes to client. But wrote " + strconv.Itoa(n) +
-						" bytes. Moving on.")
-					errChan <- err
-					return
-				} else {
-					Logger.Debugf("Successfully wrote to Client")
-				}
-			}
-		}
-		err := errors.New("Stopped")
-		errChan <- err
-	}
-
-	forwardFromClientToServer := func ()() {
-		n, err := io.Copy(TDstate.servConn, TDstate.userConn)
+		TDstate.userConn.SetWriteDeadline(time.Now().Add(time.Second * 5))
+		n, _err := io.Copy(TDstate.userConn, TDstate.servConn)
 		Logger.Debugf("[Flow " + strconv.FormatUint(uint64(TDstate.id), 10) +
 			"] forwardFromClientToServer returns, bytes sent: " +
 			strconv.FormatUint(uint64(n), 10))
-		errChan <- err
+		if _err == nil {
+			_err = errors.New("!!!Server returned without error")
+		}
+		errChan <- _err
+	}
+
+	forwardFromClientToServer := func ()() {
+		n, _err := io.Copy(TDstate.servConn, TDstate.userConn)
+		Logger.Debugf("[Flow " + strconv.FormatUint(uint64(TDstate.id), 10) +
+			"] forwardFromClientToServer returns, bytes sent: " +
+			strconv.FormatUint(uint64(n), 10))
+		errChan <- _err
+		if _err == nil {
+			_err = errors.New("StoppedByUser")
+		}
 		return
 	}
 
 	go forwardFromServerToClient()
 	go forwardFromClientToServer()
 
-	if err := <-errChan; err != nil && err.Error() != "MSG_CLOSE" {
-		str_err := err.Error()
+	if err := <-errChan; err != nil {
+		if err.Error() != "MSG_CLOSE" && err.Error() != "StoppedByUser" {
+			TDstate.proxy.closedGracefully.inc()
+		} else {
+			str_err := err.Error()
 
-		// statistics
-		if strings.Contains(str_err, "TapDance station didn't pick up the request") {
-			TDstate.proxy.notPickedUp.inc()
-		} else if strings.Contains(str_err, ": i/o timeout") {
-			TDstate.proxy.timedOut.inc()
-		}  else {
-			TDstate.proxy.unexpectedError.inc()
+			// statistics
+			if strings.Contains(str_err, "TapDance station didn't pick up the request") {
+				TDstate.proxy.notPickedUp.inc()
+			} else if strings.Contains(str_err, ": i/o timeout") {
+				TDstate.proxy.timedOut.inc()
+			} else {
+				TDstate.proxy.unexpectedError.inc()
+			}
+
+			Logger.Errorf("[Flow " + strconv.FormatUint(uint64(TDstate.id), 10) +
+				"] Redirect function returns, error: " + err.Error())
+			return err
 		}
-
-		Logger.Errorf("[Flow " + strconv.FormatUint(uint64(TDstate.id), 10)  +
-			"] Redirect function returns, error: " + err.Error())
-		return err
-	} else {
-		TDstate.proxy.closedGracefully.inc()
 	}
 	return nil
 }
