cfbecba33c5a9ce5d4c6629a869c9940050b5e5d
Sergey Frolov <sergey.frolov@colorado.edu>
2017-02-20 15:41:55 -0700

    Counter: defer Mutex Unlock

--- tapdance/counter.go
+++ tapdance/counter.go
@@ -9,31 +9,31 @@ type counter_uint64 struct {
 
 func (c *counter_uint64) inc() (uint64) {
 	c.Lock()
+	defer c.Unlock()
 	if c.value == ^uint64(0) {
 		// if max
 		c.value = 0
 	} else {
 		c.value++
 	}
-	c.Unlock()
 	return c.value
 }
 
 func (c *counter_uint64) dec() (uint64) {
 	c.Lock()
+	defer c.Unlock()
 	if c.value == 0 {
 		c.value = ^uint64(0)
 	} else {
 		c.value--
 	}
-	c.Unlock()
 	return c.value
 }
 
 func (c *counter_uint64) get() (value uint64) {
 	c.Lock()
+	defer c.Unlock()
 	value = c.value
-	c.Unlock()
 	return
 }
 
