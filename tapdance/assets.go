d185659d9649d34c83f3977478ac39768d164021
Sergey Frolov <sergey.frolov@colorado.edu>
2017-03-01 14:11:25 -0700

    GetPubkey: return pointer + cosmetics

--- tapdance/assets.go
+++ tapdance/assets.go
@@ -27,10 +27,6 @@ type assets struct {
 	roots         *x509.CertPool
 }
 
-var td_station_pubkey = [32]byte{211, 127, 10, 139, 150, 180, 97, 15, 56, 188, 7, 155, 7, 102,
-	41, 34, 70, 194, 210, 170, 50, 53, 234, 49, 42, 240, 41, 27, 91, 38, 247, 67}
-
-
 var assetsInstance *assets
 var assetsOnce sync.Once
 
@@ -85,26 +81,26 @@ func (a *assets) SetAssetsDir(path string) {
 }
 
 func (a *assets) readConfigs() {
-	keyfile := a.path + "station_pubkey"
-	caList := a.path + "roots"
-	//decoyList := a.path + "decoys"
+	pubkeyFilename := a.path + "station_pubkey"
+	rootsFilename := a.path + "roots"
+	//decoysFilename := a.path + "decoys"
 
-	sliceStaionPubkey, err := ioutil.ReadFile(keyfile)
+	staionPubkey, err := ioutil.ReadFile(pubkeyFilename)
 	if err != nil {
 		Logger.Errorln("Could not read keyfile: " + err.Error())
-	} else if len(sliceStaionPubkey) != 32 {
+	} else if len(staionPubkey) != 32 {
 		Logger.Errorln("Unexpected keyfile length! Expected: 32. Got: " +
-			strconv.Itoa(len(sliceStaionPubkey)))
+			strconv.Itoa(len(staionPubkey)))
 	} else {
-		copy(a.stationPubkey[:], sliceStaionPubkey[0:32])
+		copy(a.stationPubkey[:], staionPubkey[0:32])
 	}
 
-	sliceStaionRootPem, err := ioutil.ReadFile(caList)
+	rootCerts, err := ioutil.ReadFile(rootsFilename)
 	if err != nil {
 		Logger.Errorln("Could not read root ca file: " + err.Error())
 	} else {
 		roots := x509.NewCertPool()
-		ok := roots.AppendCertsFromPEM(sliceStaionRootPem)
+		ok := roots.AppendCertsFromPEM(rootCerts)
 		if !ok {
 			Logger.Errorln("Failed to parse root certificates")
 		} else {
@@ -120,21 +116,21 @@ func (a *assets) GetDecoyAddress() (sni string, addr string) {
 	defer a.RUnlock()
 
 	decoyIndex := getRandInt(0, len(a.decoys)-1)
-	addr = a.decoys[decoyIndex].ip + ":" + strconv.Itoa(443)
+	addr = a.decoys[decoyIndex].ip + ":443"
 	sni = a.decoys[decoyIndex].sni
 	return
 }
 
-func (a *assets) GetRoots() (*x509.CertPool) {
+func (a *assets) GetRoots() *x509.CertPool {
 	a.RLock()
 	defer a.RUnlock()
 
 	return a.roots
 }
 
-func (a *assets) GetPubkey() (pubkey [32]byte) {
+func (a *assets) GetPubkey() *[32]byte {
 	a.RLock()
 	defer a.RUnlock()
 
-	return a.stationPubkey
+	return &(a.stationPubkey)
 }
