e6f6bd22b4993133f88bb11c228d8038c0fefdd4
Sergey Frolov <sergey.frolov@colorado.edu>
2017-05-04 11:42:06 -0600

    Declare rConn and wConn

---
+++ tapdance/tdRConn.go
@@ -0,0 +1,6 @@
+package tapdance
+
+// Establishes initial connection to TapDance station and acts as read flow afterwards
+type tapdanceRConn struct {
+
+}
---
+++ tapdance/tdWConn.go
@@ -0,0 +1,9 @@
+package tapdance
+
+// Used for writing data and session control messages
+type tapdanceWConn struct {
+
+}
+/*
+It probably should have read flow that reads messages and then send "STAHP" to channel when read
+ */
