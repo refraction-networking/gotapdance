de06db2fc4b6ea6c109e7538892682ee80c8f931
Sergey Frolov <sergey.frolov@colorado.edu>
2017-06-11 23:01:42 -0400

    Update cli to use new tdproxy

--- cli/main.go
+++ cli/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"github.com/SergeyFrolov/gotapdance/tapdance"
 	"flag"
 	"fmt"
+	"github.com/SergeyFrolov/gotapdance/tdproxy"
+	"os"
 )
 
 func main() {
-
 	portPtr := flag.Int("port", 10500, "port number")
 	flag.Parse()
 
-	if (*portPtr < 1 || *portPtr > 65535) {
+	if *portPtr < 1 || *portPtr > 65535 {
 		fmt.Println("Invalid port:", *portPtr)
 		return
-        }
+	}
 
-	tapdanceProxy := tapdance.NewTapdanceProxy(*portPtr)
-	tapdanceProxy.Listen()
+	tapdanceProxy := tdproxy.NewTapDanceProxy(*portPtr)
+	err := tapdanceProxy.ListenAndServe()
+	if err != nil {
+		tdproxy.Logger.Errorf("Failed to ListenAndServe(): %v\n", err)
+		os.Exit(1)
+	}
 }
