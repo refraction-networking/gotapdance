0c1451be3a719603da7fde553fcaeb49d6528fc6
Sergey Frolov <sergey.frolov@colorado.edu>
2017-09-30 19:46:14 -0600

    Revert "enabled port to be comand-line configurable"
    This reverts commit 68bee8f8327af8978ab6bb778baed9dea70ba6a9.

--- cli/main.go
+++ cli/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"github.com/pkg/profile"
 	"github.com/sergeyfrolov/gotapdance/tdproxy"
 	"os"
@@ -10,15 +8,8 @@ import (
 
 func main() {
 	defer profile.Start().Stop()
-	portPtr := flag.Int("port", 10500, "port number")
-	flag.Parse()
 
-	if *portPtr < 1 || *portPtr > 65535 {
-		fmt.Println("Invalid port:", *portPtr)
-		return
-	}
-
-	tapdanceProxy := tdproxy.NewTapDanceProxy(*portPtr)
+	tapdanceProxy := tdproxy.NewTapDanceProxy(10500)
 	err := tapdanceProxy.ListenAndServe()
 	if err != nil {
 		tdproxy.Logger.Errorf("Failed to ListenAndServe(): %v\n", err)
