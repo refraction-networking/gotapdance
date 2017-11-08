7ef6c29f48d7ae5e69b6f06f4b4100bd33ca482a
Sergey Frolov <sergey.frolov@colorado.edu>
2017-11-08 14:32:35 -0700

    Implement #55: a way to specify location of assets

--- cli/main.go
+++ cli/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/pkg/profile"
+	"github.com/sergeyfrolov/gotapdance/tapdance"
 	"github.com/sergeyfrolov/gotapdance/tdproxy"
 	"os"
-	"flag"
 )
 
 func main() {
 	defer profile.Start().Stop()
 
 	var port = flag.Int("port", 10500, "TapDance will listen for connections on this port.")
+	var assets_location = flag.String("assetsdir", "./assets/", "Folder to read assets from.")
 	flag.Parse()
+
+	tapdance.AssetsFromDir(*assets_location)
 
 	tapdanceProxy := tdproxy.NewTapDanceProxy(*port)
 	err := tapdanceProxy.ListenAndServe()
