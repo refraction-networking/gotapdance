564a41efa1ff28a0d14bb0fd8d5ed72641335830
Sergey Frolov <sergey.frolov@colorado.edu>
2017-11-08 14:17:32 -0700

    Implement #54: cli option to set the proxy port

--- cli/main.go
+++ cli/main.go
@@ -4,12 +4,16 @@ import (
 	"github.com/pkg/profile"
 	"github.com/sergeyfrolov/gotapdance/tdproxy"
 	"os"
+	"flag"
 )
 
 func main() {
 	defer profile.Start().Stop()
 
-	tapdanceProxy := tdproxy.NewTapDanceProxy(10500)
+	var port = flag.Int("port", 10500, "TapDance will listen for connections on this port.")
+	flag.Parse()
+
+	tapdanceProxy := tdproxy.NewTapDanceProxy(*port)
 	err := tapdanceProxy.ListenAndServe()
 	if err != nil {
 		tdproxy.Logger.Errorf("Failed to ListenAndServe(): %v\n", err)
