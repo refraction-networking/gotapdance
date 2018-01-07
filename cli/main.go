c3e4f4fda760dd86e06093322c0637398af0b928
Ben VanderSloot <benvds@umich.edu>
2018-01-07 10:13:23 -0500

    Adding flags for debugging

--- cli/main.go
+++ cli/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"github.com/pkg/profile"
-	"github.com/sergeyfrolov/gotapdance/tdproxy"
+	"flag"
 	"os"
+	"strings"
+
+	"github.com/pkg/profile"
+	"github.com/sergeyfrolov/gotapdance/tapdance"
+	"github.com/sergeyfrolov/gotapdance/tdproxy"
 )
 
 func main() {
 	defer profile.Start().Stop()
 
-	tapdanceProxy := tdproxy.NewTapDanceProxy(10500)
+	portPtr := flag.Int("p", 10500, "HTTP proxy port")
+	hostPtr := flag.String("h", "tapdance2.freeaeskey.xyz", "overt host to request resources from")
+	resourcesPtr := flag.String("r", "/large-file.dat", "comma separated list of resources to request")
+
+	tapdanceProxy := tdproxy.NewTapDanceProxy(*portPtr)
+	tapdance.OvertHost = *hostPtr
+	tapdance.OvertResources = strings.Split(*resourcesPtr, ",")
 	err := tapdanceProxy.ListenAndServe()
 	if err != nil {
 		tdproxy.Logger.Errorf("Failed to ListenAndServe(): %v\n", err)
