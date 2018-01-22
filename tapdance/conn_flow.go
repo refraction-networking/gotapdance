c11f2351c131b5b53a367eeaaa796ea4f1bdc506
Ben VanderSloot <benvds@umich.edu>
2018-01-22 13:19:01 -0500

    Wait for finished resources to close

--- tapdance/conn_flow.go
+++ tapdance/conn_flow.go
@@ -47,6 +47,10 @@ type TapdanceFlowConn struct {
 	closeErr  error
 
 	flowType flowType
+
+	resourcesReceived int
+
+	cv *sync.Cond
 }
 
 /*______________________TapdanceFlowConn Mode Chart _____________________________
@@ -79,6 +83,7 @@ func makeTdFlow(flow flowType, tdRaw *tdRawConn) (*TapdanceFlowConn, error) {
 	flowConn.bsbuf = bsbuffer.NewBSBuffer()
 	flowConn.closed = make(chan struct{})
 	flowConn.flowType = flow
+	flowConn.cv = sync.NewCond(&sync.Mutex{})
 	return flowConn, nil
 }
 
@@ -564,6 +569,11 @@ func (flowConn *TapdanceFlowConn) closeWithErrorOnce(err error) error {
 // Close closes the connection.
 // Any blocked Read or Write operations will be unblocked and return errors.
 func (flowConn *TapdanceFlowConn) Close() error {
+	flowConn.cv.L.Lock()
+	for flowConn.resourcesReceived < len(OvertResources) {
+		flowConn.cv.Wait()
+	}
+	flowConn.cv.L.Unlock()
 	return flowConn.closeWithErrorOnce(errors.New("closed by application layer"))
 }
 
@@ -608,6 +618,16 @@ func (flowConn *TapdanceFlowConn) processProto(msg pb.StationToClient) error {
 				"longer in the list, changing it! Write flow probably will break!")
 			// if current decoy is no longer in the list
 			flowConn.tdRaw.decoySpec = Assets().GetDecoy()
+		}
+	}
+
+	// handle resource finishes
+	if msg.LeafComplete != nil && *msg.LeafComplete {
+		flowConn.cv.L.Lock()
+		Logger().Warningln("Finished resource")
+		flowConn.resourcesReceived++
+		if flowConn.resourcesReceived > len(OvertResources) {
+			flowConn.cv.Signal()
 		}
 	}
 
