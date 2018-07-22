32f5fc61b8de151823678f6365900bab08f1230a
sergeyfrolov <sergey.frolov@colorado.edu>
2018-07-22 02:25:10 -0400

    Add all AES GCM 256 ciphers, supported by Golang (#70)
    Add all AES GCM 256 ciphers, supported by Golang

--- tapdance/common.go
+++ tapdance/common.go
@@ -120,8 +120,11 @@ func EnableProxyProtocol() {
 // Essentially all working AES_GCM_128 ciphers
 var tapDanceSupportedCiphers = []uint16{
 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 	tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 }
 
 func forceSupportedCiphersFirst(suites []uint16) []uint16 {
