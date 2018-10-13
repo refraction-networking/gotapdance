0f832d668534836750dd413b105bbc3cd0807257
Sze Chuen Tan <tan@szechuen.com>
2018-10-13 15:09:00 -0500

    Tabs ugh (2)

--- tapdance/probe.go
+++ tapdance/probe.go
@@ -1,17 +1,17 @@
 package tapdance
 
 import (
-  "context"
-  pb "github.com/sergeyfrolov/gotapdance/protobuf"
+	"context"
+	pb "github.com/sergeyfrolov/gotapdance/protobuf"
 )
 
 func ProbeDecoy(ip string, sni string) (err error) {
-  flow, _ := makeTdFlow(flowBidirectional, nil)
+	flow, _ := makeTdFlow(flowBidirectional, nil)
 
-  flow.tdRaw.decoySpec = *pb.InitTLSDecoySpec(ip, sni)
-  flow.tdRaw.pinDecoySpec = true
+	flow.tdRaw.decoySpec = *pb.InitTLSDecoySpec(ip, sni)
+	flow.tdRaw.pinDecoySpec = true
 
-  err = flow.tdRaw.tryDialOnce(context.Background(), pb.S2C_Transition_S2C_SESSION_INIT)
-  _ = flow.tdRaw.Close()
-  return
+	err = flow.tdRaw.tryDialOnce(context.Background(), pb.S2C_Transition_S2C_SESSION_INIT)
+	_ = flow.tdRaw.Close()
+	return
 }
