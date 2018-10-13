4ad5f3e541feec88b24bae1b86a0c87ddf806ce1
Sze Chuen Tan <tan@szechuen.com>
2018-10-13 15:05:40 -0500

    Tabs ugh

--- probe/main.go
+++ probe/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
-  "github.com/sergeyfrolov/gotapdance/tapdance"
+	"flag"
+	"fmt"
+	"github.com/sergeyfrolov/gotapdance/tapdance"
+	"os"
 )
 
 func main() {
-  var scan = flag.Bool("scan", false, "Enable scan mode to probe multiple decoys")
-  flag.Parse()
+	var scan = flag.Bool("scan", false, "Enable scan mode to probe multiple decoys")
+	flag.Parse()
 
-  if *scan {
-    if flag.NArg() != 1 {
-      fmt.Println(os.Args[0], "-scan <file>")
-      os.Exit(255)
-    }
-    _ = flag.Arg(0)
+	if *scan {
+		if flag.NArg() != 1 {
+			fmt.Println(os.Args[0], "-scan <file>")
+			os.Exit(255)
+		}
+		_ = flag.Arg(0)
 
-    fmt.Println("To be implemented")
-  } else {
-    if flag.NArg() != 2 {
-      fmt.Println(os.Args[0], "<ip> <sni>")
-      os.Exit(255)
-    }
-    ip := flag.Arg(0)
-    sni := flag.Arg(1)
+		fmt.Println("To be implemented")
+	} else {
+		if flag.NArg() != 2 {
+			fmt.Println(os.Args[0], "<ip> <sni>")
+			os.Exit(255)
+		}
+		ip := flag.Arg(0)
+		sni := flag.Arg(1)
 
-    err := tapdance.ProbeDecoy(ip, sni)
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
-  }
+		err := tapdance.ProbeDecoy(ip, sni)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
 }
