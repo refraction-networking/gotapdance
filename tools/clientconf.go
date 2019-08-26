69a82aa87c8094101f95e22e9e150ca6647288ca
Jack W <jack.wampler@colorado.edu>
2019-08-26 13:50:33 -0600

    added ability to send special packet to all registration decoys to test which are routes pass the station.

--- tools/clientconf.go
+++ tools/clientconf.go
@@ -1,105 +1,24 @@
 package main
 
 import (
-	"encoding/binary"
-	"encoding/hex"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 
 	"github.com/golang/protobuf/proto"
 	pb "github.com/sergeyfrolov/gotapdance/protobuf"
 )
 
-func printClientConf(clientConf pb.ClientConf) {
-	fmt.Printf("Generation: %d\n", clientConf.GetGeneration())
-	if clientConf.GetDefaultPubkey() != nil {
-		fmt.Printf("\nDefault Pubkey: %s\n", hex.EncodeToString(clientConf.GetDefaultPubkey().Key[:]))
-	}
-	if clientConf.DecoyList == nil {
-		return
-	}
-	decoys := clientConf.DecoyList.TlsDecoys
-	fmt.Printf("\nDecoy List: %d decoys\n", len(decoys))
-	for i, decoy := range decoys {
-		ip := make(net.IP, 4)
-		binary.BigEndian.PutUint32(ip, decoy.GetIpv4Addr())
-		fmt.Printf("%d:\n  %s (%s)\n", i, decoy.GetHostname(), ip.To4().String())
-		if decoy.GetPubkey() != nil {
-			fmt.Printf("  pubkey: %s\n", hex.EncodeToString(decoy.GetPubkey().Key[:]))
-		}
-		if decoy.GetTimeout() != 0 {
-			fmt.Printf("  timeout: %d ms\n", decoy.GetTimeout())
-		}
-		if decoy.GetTcpwin() != 0 {
-			fmt.Printf("  tcpwin: %d bytes\n", decoy.GetTcpwin())
-		}
-	}
-	if clientConf.GetDarkDecoyBlocks() != nil {
-		darkDecoyBlocks := clientConf.DarkDecoyBlocks.Blocks
-		fmt.Printf("\nDark Decoy Blocks: %d blocks\n", len(darkDecoyBlocks))
-		for i, block := range darkDecoyBlocks {
-			fmt.Printf("%d:\n  %s\n", i, block)
-		}
-	}
-}
-
-func parseClientConf(fname string) pb.ClientConf {
-
-	clientConf := pb.ClientConf{}
-	buf, err := ioutil.ReadFile(fname)
-	if err != nil {
-		log.Fatal("Error reading file:", err)
-	}
-	err = proto.Unmarshal(buf, &clientConf)
-	if err != nil {
-		log.Fatal("Error parsing ClientConf", err)
-	}
-	return clientConf
-}
-
-func parsePubkey(pubkey string) []byte {
-	pubkey_bin, err := hex.DecodeString(pubkey)
-	if err != nil {
-		log.Fatal("Error parsing pubkey:", err)
-	}
-	if len(pubkey_bin) != 32 {
-		log.Fatal("Error: pubkey length: expected 32, got ", len(pubkey_bin))
-	}
-	return pubkey_bin
-
-}
-
-func updateDecoy(decoy *pb.TLSDecoySpec, host string, ip string, pubkey string, delpubkey bool, timeout int, tcpwin int) {
-
-	if host != "" {
-		decoy.Hostname = &host
-	}
-	if ip != "" {
-		ip4 := binary.BigEndian.Uint32(net.ParseIP(ip).To4())
-		decoy.Ipv4Addr = &ip4
-	}
-	if pubkey != "" {
-		decoy.Pubkey.Key = parsePubkey(pubkey)
-	}
-	if delpubkey {
-		decoy.Pubkey = nil
-	}
-	if timeout != 0 {
-		t := uint32(timeout)
-		decoy.Timeout = &t
-	}
-	if tcpwin != 0 {
-		t := uint32(tcpwin)
-		decoy.Tcpwin = &t
-	}
-}
-
 func main() {
 	var fname = flag.String("f", "", "`ClientConf` file to parse")
 	var out_fname = flag.String("o", "", "`output` file name to write new/modified config")
+
+	var connect_to_all = flag.Bool("connect_to_all", false, "")
+	var workers = flag.Uint("w", 20, "")
+
 	var generation = flag.Int("generation", 0, "New/modified generation")
 	var pubkey = flag.String("pubkey", "", "New/modified (decoy) pubkey. If -add or -update, applies to specific decoy. If -all applies to all decoys. Otherwise, applies to default pubkey.")
 	var delpubkey = flag.Bool("delpubkey", false, "Delete pubkey from decoy with index specified in -update (or from all decoys if -all)")
@@ -126,6 +45,11 @@ func main() {
 	// Parse ClientConf
 	if *fname != "" {
 		clientConf = parseClientConf(*fname)
+	}
+
+	if *connect_to_all {
+		ConnectToAll(clientConf.DecoyList.GetTlsDecoys(), *workers)
+		os.Exit(0)
 	}
 
 	// Update generation
---
+++ tools/common.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	pb "github.com/sergeyfrolov/gotapdance/protobuf"
+)
+
+func printClientConf(clientConf pb.ClientConf) {
+	fmt.Printf("Generation: %d\n", clientConf.GetGeneration())
+	if clientConf.GetDefaultPubkey() != nil {
+		fmt.Printf("\nDefault Pubkey: %s\n", hex.EncodeToString(clientConf.GetDefaultPubkey().Key[:]))
+	}
+	if clientConf.DecoyList == nil {
+		return
+	}
+	decoys := clientConf.DecoyList.TlsDecoys
+	fmt.Printf("\nDecoy List: %d decoys\n", len(decoys))
+	for i, decoy := range decoys {
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, decoy.GetIpv4Addr())
+		fmt.Printf("%d:\n  %s (%s)\n", i, decoy.GetHostname(), ip.To4().String())
+		if decoy.GetPubkey() != nil {
+			fmt.Printf("  pubkey: %s\n", hex.EncodeToString(decoy.GetPubkey().Key[:]))
+		}
+		if decoy.GetTimeout() != 0 {
+			fmt.Printf("  timeout: %d ms\n", decoy.GetTimeout())
+		}
+		if decoy.GetTcpwin() != 0 {
+			fmt.Printf("  tcpwin: %d bytes\n", decoy.GetTcpwin())
+		}
+	}
+	if clientConf.GetDarkDecoyBlocks() != nil {
+		darkDecoyBlocks := clientConf.DarkDecoyBlocks.Blocks
+		fmt.Printf("\nDark Decoy Blocks: %d blocks\n", len(darkDecoyBlocks))
+		for i, block := range darkDecoyBlocks {
+			fmt.Printf("%d:\n  %s\n", i, block)
+		}
+	}
+}
+
+func parseClientConf(fname string) pb.ClientConf {
+
+	clientConf := pb.ClientConf{}
+	buf, err := ioutil.ReadFile(fname)
+	if err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+	err = proto.Unmarshal(buf, &clientConf)
+	if err != nil {
+		log.Fatal("Error parsing ClientConf", err)
+	}
+	return clientConf
+}
+
+func parsePubkey(pubkey string) []byte {
+	pubkey_bin, err := hex.DecodeString(pubkey)
+	if err != nil {
+		log.Fatal("Error parsing pubkey:", err)
+	}
+	if len(pubkey_bin) != 32 {
+		log.Fatal("Error: pubkey length: expected 32, got ", len(pubkey_bin))
+	}
+	return pubkey_bin
+
+}
+
+func updateDecoy(decoy *pb.TLSDecoySpec, host string, ip string, pubkey string, delpubkey bool, timeout int, tcpwin int) {
+
+	if host != "" {
+		decoy.Hostname = &host
+	}
+	if ip != "" {
+		ip4 := binary.BigEndian.Uint32(net.ParseIP(ip).To4())
+		decoy.Ipv4Addr = &ip4
+	}
+	if pubkey != "" {
+		decoy.Pubkey.Key = parsePubkey(pubkey)
+	}
+	if delpubkey {
+		decoy.Pubkey = nil
+	}
+	if timeout != 0 {
+		t := uint32(timeout)
+		decoy.Timeout = &t
+	}
+	if tcpwin != 0 {
+		t := uint32(tcpwin)
+		decoy.Tcpwin = &t
+	}
+}
+
+type jobTuple struct {
+	Decoy *pb.TLSDecoySpec
+	Total uint
+	JobId uint
+}
+
+func ConnectToAll(decoyList []*pb.TLSDecoySpec, workers uint) {
+
+	fmt.Println("Connection to all registration decoys in client conf.")
+
+	decoyChan := make(chan jobTuple)
+
+	for j := uint(1); j <= workers; j++ {
+		go ConnectWorker(j, decoyChan)
+	}
+
+	for idx := 0; idx < len(decoyList); idx++ {
+		decoyChan <- jobTuple{Decoy: decoyList[idx], Total: uint(len(decoyList)), JobId: uint(idx)}
+	}
+}
+
+func ConnectWorker(id uint, decoys <-chan jobTuple) {
+	jobsCompleted := 0
+	var decoy *pb.TLSDecoySpec
+	var err error
+
+	for decoyTuple := range decoys {
+		jobsCompleted++
+
+		decoy = decoyTuple.Decoy
+		fmt.Printf("[%d/%d/%d] (%d) Connecting to decoy %s -- %s ... \n",
+			jobsCompleted, decoyTuple.JobId, decoyTuple.Total, id, decoy.GetHostname(), decoy.GetIpAddrStr())
+
+		err = ConnectSpecial(decoy)
+		if err != nil {
+			fmt.Printf("[%d/%d/%d] (%d) Failure: %s\n",
+				jobsCompleted, decoyTuple.JobId, decoyTuple.Total, id, err.Error())
+		} else {
+			fmt.Printf("[%d/%d/%d] (%d) Success\n",
+				jobsCompleted, decoyTuple.JobId, decoyTuple.Total, id)
+		}
+	}
+}
+
+func ConnectSpecial(decoy *pb.TLSDecoySpec) error {
+
+	timeout := time.Duration(5 * time.Second)
+
+	d := net.Dialer{Timeout: timeout}
+	conn, err := d.Dial("tcp", decoy.GetIpAddrStr())
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(conn, "'This must be Thursday,' said Arthur to himself, sinking low over his beer. 'I never could get the hang of Thursdays.'")
+
+	conn.Close()
+	return nil
+}
