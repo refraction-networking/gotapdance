8df876a514cc2989bf0b179422139fa9774e4adc
Mingye Chen <mingyechen82@gmail.com>
2022-05-20 10:15:12 -0400

    Add domain flag

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net"
 	"os"
+
+	"github.com/mingyech/conjure-dns-registrar/pkg/dns"
 )
 
 func main() {
@@ -19,10 +21,21 @@ func main() {
 	}
 
 	var remoteAddr net.Addr
+	var pconn net.PacketConn
 	remoteAddr, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	domain, err := dns.ParseName(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "invalid domain %+q: %v\n", flag.Arg(0), err)
+		os.Exit(1)
+	}
+
+	pconn, err = net.ListenUDP("udp", nil)
+
 	fmt.Println("addr: ", remoteAddr)
+
+	pconn = NewDNSPacketConn(pconn, remoteAddr, domain)
 }
