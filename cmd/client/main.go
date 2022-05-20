5e2f26641bbffcfb2baf0ac3a092449c14a60677
Mingye Chen <mingyechen82@gmail.com>
2022-05-20 13:45:59 -0400

    Add run func

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,6 +10,59 @@ import (
 	"github.com/mingyech/conjure-dns-registrar/pkg/dns"
 )
 
+// dnsNameCapacity returns the number of bytes remaining for encoded data after
+// including domain in a DNS name.
+func dnsNameCapacity(domain dns.Name) int {
+	// Names must be 255 octets or shorter in total length.
+	// https://tools.ietf.org/html/rfc1035#section-2.3.4
+	capacity := 255
+	// Subtract the length of the null terminator.
+	capacity -= 1
+	for _, label := range domain {
+		// Subtract the length of the label and the length octet.
+		capacity -= len(label) + 1
+	}
+	// Each label may be up to 63 bytes long and requires 64 bytes to
+	// encode.
+	capacity = capacity * 63 / 64
+	// Base32 expands every 5 bytes to 8.
+	capacity = capacity * 5 / 8
+	return capacity
+}
+
+func run(domain dns.Name, localAddr *net.TCPAddr, remoteAddr net.Addr, pconn net.PacketConn) error {
+	defer pconn.Close()
+
+	ln, err := net.ListenTCP("tcp", localAddr)
+	if err != nil {
+		return fmt.Errorf("opening local listener: %v", err)
+	}
+	defer ln.Close()
+
+	mtu := dnsNameCapacity(domain) - 8 - 1 - numPadding - 1 // clientid + padding length prefix + padding + data length prefix
+	if mtu < 80 {
+		return fmt.Errorf("domain %s leaves only %d bytes for payload", domain, mtu)
+	}
+	log.Printf("effective MTU %d", mtu)
+
+	for {
+		local, err := ln.Accept()
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Temporary() {
+				continue
+			}
+			return err
+		}
+		go func() {
+			defer local.Close()
+			err := handle(local.(*net.TCPConn), sess, conn.GetConv())
+			if err != nil {
+				log.Printf("handle: %v", err)
+			}
+		}()
+	}
+}
+
 func main() {
 	var addr string
 	flag.StringVar(&addr, "addr", "", "address of DNS resolver")
@@ -27,6 +80,12 @@ func main() {
 		log.Fatal(err)
 	}
 
+	localAddr, err := net.ResolveTCPAddr("tcp", flag.Arg(1))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	domain, err := dns.ParseName(flag.Arg(0))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "invalid domain %+q: %v\n", flag.Arg(0), err)
@@ -38,4 +97,8 @@ func main() {
 	fmt.Println("addr: ", remoteAddr)
 
 	pconn = NewDNSPacketConn(pconn, remoteAddr, domain)
+	err = run(domain, localAddr, remoteAddr, pconn)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
