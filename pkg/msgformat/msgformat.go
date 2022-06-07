4c0d0fbdb18c56dae578365aab1b3de1256a3595
Mingye Chen <mingyechen82@gmail.com>
2022-06-07 18:06:15 -0400

    Add msg format helper functions

---
+++ pkg/msgformat/msgformat.go
@@ -0,0 +1,21 @@
+package msgformat
+
+import (
+	"errors"
+)
+
+// Prefix length to message
+func AddFormat(p []byte) ([]byte, error) {
+	length := uint8(len(p))
+	prefixed := append([]byte{length}, p...)
+	return prefixed, nil
+}
+
+// Read the first byte as length of message and return the message accordingly
+func RemoveFormat(p []byte) ([]byte, error) {
+	length := uint8(p[0])
+	if int(1+length) > len(p) {
+		return nil, errors.New("invalid message length recieved")
+	}
+	return p[1 : 1+length], nil
+}
