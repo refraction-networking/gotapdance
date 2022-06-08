26820443a48305c973ad35ec2ce45a1f34852df0
Mingye Chen <mingyechen82@gmail.com>
2022-06-08 15:26:15 -0400

    Check for empty payload while removing format

--- pkg/msgformat/msgformat.go
+++ pkg/msgformat/msgformat.go
@@ -13,9 +13,12 @@ func AddFormat(p []byte) ([]byte, error) {
 
 // Read the first byte as length of message and return the message accordingly
 func RemoveFormat(p []byte) ([]byte, error) {
+	if len(p) < 1 {
+		return nil, errors.New("invalid message length")
+	}
 	length := uint8(p[0])
 	if int(1+length) > len(p) {
-		return nil, errors.New("invalid message length recieved")
+		return nil, errors.New("invalid message length")
 	}
 	return p[1 : 1+length], nil
 }
