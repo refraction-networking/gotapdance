53649424cfd0a8c4bc155ff76b792129cb09ecee
jmwample <jack.wampler@colorado.edu>
2022-06-30 10:34:16 -0600

    tighten up phantom selection to prevent div by 0 and support small allocs

--- tapdance/phantoms/phantoms_test.go
+++ tapdance/phantoms/phantoms_test.go
@@ -2,6 +2,7 @@ package phantoms
 
 import (
 	"encoding/hex"
+	"fmt"
 	"math/rand"
 	"net"
 	"testing"
@@ -10,27 +11,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestIPSelectionAlt(t *testing.T) {
+func TestIPSelectionBasic(t *testing.T) {
 
 	seed, err := hex.DecodeString("5a87133b68da3468988a21659a12ed2ece07345c8c1a5b08459ffdea4218d12f")
-	if err != nil {
-		t.Fatalf("Issue decoding seedStr")
-	}
+	require.Nil(t, err)
 
-	//netStr := "192.122.190.0/24"
 	netStr := "2001:48a8:687f:1::/64"
 	_, net1, err := net.ParseCIDR(netStr)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.Nil(t, err)
 
 	addr, err := SelectAddrFromSubnet(seed, net1)
-	if err != nil {
-		t.Fatal(err)
-	} else if addr.String() != "2001:48a8:687f:1:5fa4:c34c:434e:ddd" {
-		t.Fatalf("Wrong Address Selected: %v -> expected (%v)", addr, "2001:48a8:687f:1:5fa4:c34c:434e:ddd")
-	}
-
+	require.Nil(t, err)
+	require.Equal(t, "2001:48a8:687f:1:5fa4:c34c:434e:ddd", addr.String())
 }
 
 func TestSelectWeightedMany(t *testing.T) {
@@ -115,34 +107,26 @@ var w1 = uint32(1)
 var w9 = uint32(9)
 var phantomSubnets = &pb.PhantomSubnetsList{
 	WeightedSubnets: []*pb.PhantomSubnets{
-		{Weight: &w9, Subnets: []string{"192.122.190.0/24", "2001:48a8:687f:1::/64"}},
+		{Weight: &w9, Subnets: []string{"192.122.190.0/24", "10.0.0.0/31", "2001:48a8:687f:1::/64"}},
 		{Weight: &w1, Subnets: []string{"141.219.0.0/16", "35.8.0.0/16"}},
 	},
 }
 
 func TestSelectFilter(t *testing.T) {
 	seed, err := hex.DecodeString("5a87133b68ea3468988a21659a12ed2ece07345c8c1a5b08459ffdea4218d12f")
-	if err != nil {
-		t.Fatalf("Issue decoding seedStr")
-	}
+	require.Nil(t, err)
 
 	p, err := SelectPhantomWeighted([]byte(seed), phantomSubnets, V4Only)
-	if err != nil {
-		t.Fatalf("Failed to select phantom: %v", err)
-	}
-	t.Logf("%v\n", p)
+	require.Nil(t, err)
+	require.Equal(t, "192.122.190.130", p.String())
 
 	p, err = SelectPhantomWeighted([]byte(seed), phantomSubnets, V6Only)
-	if err != nil {
-		t.Fatalf("Failed to select phantom: %v", err)
-	}
-	t.Logf("%v\n", p)
+	require.Nil(t, err)
+	require.Equal(t, "2001:48a8:687f:1:5fa4:c34c:434e:ddd", p.String())
 
 	p, err = SelectPhantomWeighted([]byte(seed), phantomSubnets, nil)
-	if err != nil {
-		t.Fatalf("Failed to select phantom: %v", err)
-	}
-	t.Logf("%v\n", p)
+	require.Nil(t, err)
+	require.Equal(t, "2001:48a8:687f:1:5fa4:c34c:434e:ddd", p.String())
 }
 
 func TestPhantomsV6OnlyFilter(t *testing.T) {
@@ -154,4 +138,53 @@ func TestPhantomsV6OnlyFilter(t *testing.T) {
 	testNetsParsed, err = V6Only(testNetsParsed)
 	require.Nil(t, err)
 	require.Equal(t, 2, len(testNetsParsed))
+}
+
+// TestPhantomsSeededSelectionV4Min ensures that minimal subnets work because
+// they re useful to test limitations (i.e. multiple clients sharing a phantom
+// address)
+func TestPhantomsSeededSelectionV4Min(t *testing.T) {
+	subnets, err := parseSubnets([]string{"192.122.190.0/32", "2001:48a8:687f:1::/128"})
+	require.Nil(t, err)
+
+	seed, err := hex.DecodeString("5a87133b68ea3468988a21659a12ed2ece07345c8c1a5b08459ffdea4218d12f")
+	require.Nil(t, err)
+
+	phantomAddr, err := selectIPAddr(seed, subnets)
+	require.Nil(t, err)
+
+	possibleAddrs := []string{"192.122.190.0", "2001:48a8:687f:1::"}
+	require.Contains(t, possibleAddrs, phantomAddr.String())
+}
+
+// TestPhantomSeededSelectionFuzz ensures that all phantom subnet sizes are
+// viable including small (/31, /32, etc.) subnets which were previously
+// experiencing a divide by 0.
+func TestPhantomSeededSelectionFuzz(t *testing.T) {
+	_, defaultV6, err := net.ParseCIDR("2001:48a8:687f:1::/64")
+	require.Nil(t, err)
+
+	var randSeed int64 = 1234
+	r := rand.New(rand.NewSource(randSeed))
+
+	// Add generation with only one v4 subnet that has a varying mask len
+	for i := 0; i <= 32; i++ {
+		s := "255.255.255.255/" + fmt.Sprint(i)
+		_, variableSubnet, err := net.ParseCIDR(s)
+		require.Nil(t, err)
+
+		subnets := []*net.IPNet{defaultV6, variableSubnet}
+
+		var seed = make([]byte, 32)
+		for j := 0; j < 10000; j++ {
+			n, err := r.Read(seed)
+			require.Nil(t, err)
+			require.Equal(t, n, 32)
+
+			// phantomAddr, err := phantomSelector.Select(seed, newGen, false)
+			phantomAddr, err := selectIPAddr(seed, subnets)
+			require.Nil(t, err, "i=%d, j=%d, seed='%s'", i, j, hex.EncodeToString(seed))
+			require.NotNil(t, phantomAddr)
+		}
+	}
 }
