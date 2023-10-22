f42ff661e61056d7aa5156c899cec6589b2f072c
0x90-n <cs.alaraj@gmail.com>
2023-10-22 03:09:57 -0600

    print registration result to stdout instead of stderr

--- tapdance/conjure.go
+++ tapdance/conjure.go
@@ -67,7 +67,7 @@ func DialConjure(ctx context.Context, cjSession *ConjureSession, registrationMet
 	}
 
 	if registerOnly {
-		Logger().Printf("%v Successfully registered to use IPv4 phantom: %s:%d or IPv6 phantom: %s:%d", cjSession.IDString(), registration.phantom4.String(), registration.phantomDstPort, registration.phantom6.String(), registration.phantomDstPort)
+		fmt.Printf("%v Successfully registered to use IPv4 phantom: %s:%d or IPv6 phantom: %s:%d", cjSession.IDString(), registration.phantom4.String(), registration.phantomDstPort, registration.phantom6.String(), registration.phantomDstPort)
 		return nil, err
 	}
 
